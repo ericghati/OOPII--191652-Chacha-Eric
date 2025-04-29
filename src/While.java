cab19f82ecbbf5db5c2e89c72fdc2527a3a3ae0f
Eric Ghati <ericghati@gmail.com>
2025-04-29 13:43:16 +0300

    While loop to display even numbers. It starts from 50 till 1 and checks if divisible by 2 using a modulus. For the period that our condition is met the value is displayed. And since we know that even numbers have one odd between we skip the odd by decrementing by 2.

---
+++ src/While.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class While {
+
+    Scanner sc = new Scanner(System.in);
+
+    void CheckNumber(){
+        int number = 50;
+        int even;
+
+        while (number % 2 == 0 && number > 1) {
+            System.out.println(" " + number);
+            number -= 2;
+        }
+    }
+        public static void main(String[] args)
+        {
+        While number = new While();
+        number.CheckNumber();
+        }
+
+}
