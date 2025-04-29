f1e56ed1179cbf73e6f8e7f1070b3b5ebbbb720d
Eric Ghati <ericghati@gmail.com>
2025-04-29 13:20:02 +0300

    Random challenge. It allows a user to select number 1 to 10. And checks if its same as the one selected by the computer. Note: It does not account for wrong range entry and does not automatically repeat the code once again

---
+++ src/Rand.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Rand {
+
+    void Guess() {
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+        int choice;
+
+        System.out.println("Enter number 1 to 10: ");
+        number = sc.nextInt();
+
+        choice = random.nextInt(10);
+
+        if (choice == number) {
+            System.out.println("You've guessed right!");
+        } else {
+            System.out.println("Sorry, Wrong guess, try again");
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Rand guess = new Rand();
+        guess.Guess();
+    }
+}
